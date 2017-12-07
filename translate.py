3a50b2919f5d9e9544e155deafce2ca8253ede35
stmde03 <stmde03@moravian.edu>
2017-12-07 16:53:52 -0500

    Add files via upload
    These python scripts are for the Pi to integrate with the Wiimote

---
+++ translate.py
@@ -0,0 +1,149 @@
+from wiimote import getTranslation, receive_translations
+import requests
+
+translated = []
+A = ["dot", "dash"]
+B = ["dash", "dot", "dot", "dot"]
+C = ["dash", "dot", "dash", "dot"]
+D = ["dash", "dot", "dot"]
+E = ["dot"]
+F = ["dot", "dot", "dash", "dot"]
+G = ["dash", "dash", "dot"]
+H = ["dot", "dot", "dot", "dot"]
+I = ["dot", "dot"]
+J = ["dot", "dash", "dash", "dash"]
+K = ["dash", "dot", "dash"]
+L = ["dot", "dash", "dot", "dot"]
+M = ["dash", "dash"]
+N = ["dash", "dot"]
+O = ["dash", "dash", "dash"]
+P = ["dot", "dash", "dash", "dot"]
+Q = ["dash", "dash", "dot", "dash"]
+R = ["dot", "dash", "dot"]
+S = ["dot", "dot", "dot"]
+T = ["dash"]
+U = ["dot", "dot", "dash"]
+V = ["dot", "dot", "dot", "dash"]
+W = ["dot", "dash", "dash"]
+X = ["dash", "dot", "dot", "dash"]
+Y = ["dash", "dot", "dash", "dash"]
+Z = ["dash", "dash", "dot", "dot"]
+
+
+def trans_to_eng():
+    translations = getTranslation()
+    for letters in translations:
+        if letters == (A):
+            translated.append("A")
+        if letters == (B):
+            translated.append("B")
+        if letters == (C):
+            translated.append("C")
+        if letters == (D):
+            translated.append("D")
+        if letters == (E):
+            translated.append("E")
+        if letters == (F):
+            translated.append("F")
+        if letters == (G):
+            translated.append("G")
+        if letters == (H):
+            translated.append("H")
+        if letters == (I):
+            translated.append("I")
+        if letters == (J):
+            translated.append("J")
+        if letters == (K):
+            translated.append("K")
+        if letters == (L):
+            translated.append("L")
+        if letters == (M):
+            translated.append("M")
+        if letters == (N):
+            translated.append("N")
+        if letters == (O):
+            translated.append("O")
+        if letters == (P):
+            translated.append("P")
+        if letters == (Q):
+            translated.append("Q")
+        if letters == (R):
+            translated.append("R")
+        if letters == (S):
+            translated.append("S")
+        if letters == (T):
+            translated.append("T")
+        if letters == (U):
+            translated.append("U")
+        if letters == (V):
+            translated.append("V")
+        if letters == (W):
+            translated.append("W")
+        if letters == (X):
+            translated.append("X")
+        if letters == (Y):
+            translated.append("Y")
+        if letters == (Z):
+            translated.append("Z")
+        word = ""
+        for x in translated:
+            word += x
+            print(word + " will be sent.")
+
+
+def trans_to_code(word):
+    word = word.upper()
+    for letters in word:
+        if letters == ("A"):
+            translated.append(A)
+        if letters == ("B"):
+            translated.append(B)
+        if letters == ("C"):
+            translated.append(C)
+        if letters == ("D"):
+            translated.append(D)
+        if letters == ("E"):
+            translated.append(E)
+        if letters == ("F"):
+            translated.append(F)
+        if letters == ("G"):
+            translated.append(G)
+        if letters == ("H"):
+            translated.append(H)
+        if letters == ("I"):
+            translated.append(I)
+        if letters == ("J"):
+            translated.append(J)
+        if letters == ("K"):
+            translated.append(K)
+        if letters == ("L"):
+            translated.append(L)
+        if letters == ("M"):
+            translated.append(M)
+        if letters == ("N"):
+            translated.append(N)
+        if letters == ("O"):
+            translated.append(O)
+        if letters == ("P"):
+            translated.append(P)
+        if letters == ("Q"):
+            translated.append(Q)
+        if letters == ("R"):
+            translated.append(R)
+        if letters == ("S"):
+            translated.append(S)
+        if letters == ("T"):
+            translated.append(T)
+        if letters == ("U"):
+            translated.append(U)
+        if letters == ("V"):
+            translated.append(V)
+        if letters == ("W"):
+            translated.append(W)
+        if letters == ("X"):
+            translated.append(X)
+        if letters == ("Y"):
+            translated.append(Y)
+        if letters == ("Z"):
+            translated.append(Z)
+        receive_translations(translated)
---
+++ wiimote.py
@@ -0,0 +1,78 @@
+import cwiid, time
+
+button_delay = 0.1
+print ('Please press buttons 1 + 2 on your Wiimote now ...')
+time.sleep(1)
+
+def getTranslation():
+ try:
+   wii=cwiid.Wiimote()
+ except RuntimeError:
+   print ("Cannot connect to your Wiimote. Run again and make sure you are holding buttons 1 + 2!")
+   quit()
+
+ print ('Wiimote connection established!\n')
+ print ('Go ahead and press some buttons\n')
+ print ('Press PLUS and MINUS together to disconnect and quit.\n')
+
+ time.sleep(3)
+
+ wii.rpt_mode = cwiid.RPT_BTN
+ running = True
+ translations=[]
+ letter = []
+ while running:
+  buttons = wii.state['buttons']
+  if (buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
+   print ('\nClosing connection ...')
+   wii.rumble = 1
+   time.sleep(.5)
+   wii.rumble = 0
+   time.sleep(4)
+   running = False
+  if (buttons & cwiid.BTN_DOWN):
+   if (len(letter)<=4):
+     translations.append(letter)
+     print("letter saved.")
+   else:
+     print("Your letter should be 4 or less characters. Please try again.")
+   letter=[]
+   time.sleep(button_delay)
+   if (buttons & cwiid.BTN_A):
+       print('Rumbling Now')
+       wii.rumble = 1
+       time.sleep(1)
+       wii.rumble = 0
+       letter.append('dash')
+       time.sleep(button_delay)
+
+   if (buttons & cwiid.BTN_B):
+       print ('Button B pressed')
+       letter.append('dot')
+       time.sleep(button_delay)
+   return translations
+
+def receive_translations(list):
+ try:
+  wii=wii=cwiid.Wiimote()
+ except RuntimeError:
+   print ("Cannot connect to your Wiimote.Please try again!")
+   quit()
+ letter=[]
+ word=[]
+ for letters in list:
+  for chr in letters:
+   if (chr==("dash")):
+    wii.rumble=1
+    time.sleep(1)
+    wii.rumble=0
+    time.sleep(1)
+    letter.append('-')
+   if (chr==("dot")):
+    time.sleep(2)
+    letter.append('0')
+  print("",letter)
+  letter=[]
+  wii.led = 15
+  time.sleep(1)
+  wii.led = 0
